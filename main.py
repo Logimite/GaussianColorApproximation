96aa8b6e268807031c867cc7c8214db16fb28b68
Logimite <funcoding14@gmail.com>
2024-10-20 01:19:17 -0700

    fixed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 RGB = []
 alphaList = [0.362, 1.056, -0.065, 0.821, 0.286, 1.217, 0.681]
 betaList = [442.0, 599.8, 501.1, 568.8, 530.9, 437.0, 459.0]
@@ -12,20 +14,37 @@ def xyzToRGB(xyz):
         [0.0557, -0.2040, 1.0570]
     ])
     rgb = np.dot(transformation_matrix, xyz)
-    rgb = np.clip(rgb, 0, 1)
-    rgb_255 = (rgb * 255).astype(int)
-    return rgb_255
+    whited = np.dot(rgb, [0.94811, 1, 1.08883])
+    return whited
 
 def wavelengthToRGB(wavelength):
     xVal = 0
     yVal = 0
     zVal = 0
-    for x in range (0, 2):
-        xVal += alphaList[x]**((-1/2)*(wavelength-betaList[x])*((wavelength-betaList[x]) if gammaList[x] < 0 else deltaList[x]))**2
-    for y in range (0, 1):
-        yVal += alphaList[y]**((-1/2)*(wavelength-betaList[y+4])*((wavelength-betaList[y+4]) if gammaList[y+4] < 0 else deltaList[y+4]))**2
-    for z in range (0, 1):
-        zVal += alphaList[z]**((-1/2)*(wavelength-betaList[z+6])*((wavelength-betaList[z+6]) if gammaList[z+6] < 0 else deltaList[z+6]))**2
+    for x in range (2):
+        xVal += alphaList[x]*np.exp((-1/2)*(wavelength-betaList[x])*((wavelength-betaList[x]) if gammaList[x] < 0 else deltaList[x]))**2
+    for y in range (1):
+        yVal += alphaList[y+3]*np.exp((-1/2)*(wavelength-betaList[y+3])*((wavelength-betaList[y+3]) if gammaList[y+3] < 0 else deltaList[y+4]))**2
+    for z in range (1):
+        zVal += alphaList[z+5]*np.exp((-1/2)*(wavelength-betaList[z+5])*((wavelength-betaList[z+5]) if gammaList[z+5] < 0 else deltaList[z+6]))**2
     return [xVal, yVal, zVal]
+
+plt.style.use('dark_background')
+
+wavelengths = np.linspace(380, 720, 340)
+colors = []
+
+for wl in wavelengths:
+    xyz = wavelengthToRGB(wl)
+    rgb_linear = xyzToRGB(xyz)
+    rgb_srgb = rgb_linear**2.2
+    colors.append(np.clip(rgb_srgb, 0, 1))
+
+plt.figure(figsize=(10, 2))
+plt.imshow([colors], aspect='auto', extent=[380, 780, 0, 1])
+plt.xlabel('Wavelength (nm)')
+plt.yticks([])
+plt.title('Colors (1nm intervals) vs. Wavelength')
+plt.show()
 
 print(xyzToRGB(wavelengthToRGB(395)))
